1b5f0c28a7aeeabc941dabde1362a4559e38be84
amaan287 <amaanmirza287@gmail.com>
2025-05-13 00:41:35 +0530

    everything is fine backend is running without any errors "I am just a cracked dev"

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-  email: z.string().email().min(3).max(20),
-  fullname: z.string().min(3).max(20),
-  username: z.string().min(3).max(20),
-  password: z.string().min(3).max(20),
+  email: z.string().email().min(3).max(50),
+  username: z.string().min(3).max(50),
+  password: z.string().min(3).max(50),
+  fullname: z.string().min(3).max(50),
 });
 
 export const signinSchema = z.object({
-  username: z.string().min(3).max(20),
-  password: z.string().min(3).max(20),
+  username: z.string().min(3).max(50),
+  password: z.string().min(3).max(50),
 });
 
 export const CreateRoomSchema = z.object({
